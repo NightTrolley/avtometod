2145298fe56a08058eebc3440a4c572d029151d7
Nighttrolley <gairbekov086@gmail.com>
2025-08-22 17:20:04 +0300

    change page title and clear imports in index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Header from "./components/header/header.component";
-import BodyComponent from "./components/Body/body.component";
 import {BrowserRouter} from "react-router";
 import App from "./App";
 
@@ -16,7 +14,4 @@ root.render(
     </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
