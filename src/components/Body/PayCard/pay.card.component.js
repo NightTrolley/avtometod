8875aa566394a11490ea043862fd28249bd5254e
Nighttrolley <gairbekov086@gmail.com>
2025-08-29 03:30:03 +0300

    fix pay card

--- src/components/Body/PayCard/pay.card.component.js
+++ src/components/Body/PayCard/pay.card.component.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import "./pay.card.css"
-import PayCardItem from "./pay.card.item";
+import line from "../../../icons/Line.svg";
 
 const PayCardComponent = () => {
     return (
         <div className="pay-card-block col-12">
             <div className="pay-card-item-wrapper col-12 row g-0">
-                <PayCardItem/>
-                <PayCardItem/>
-                <PayCardItem/>
+                <div className="pay-card-item row g-0 col-4">
+                    <div className="column col-10">
+                        <div className="pay-card-text-h1 sm-fs-1">
+                            без %
+                        </div>
+                        <div className="pay-card-text-h3 sm-fs-0">
+                            никаких переплат
+                        </div>
+                    </div>
+                    <div className="pay-card-item-devider">
+                        <img src={line} alt=""/>
+                    </div>
+                </div>
+                <div className="pay-card-item row g-0 col-4">
+                    <div className="column col-10">
+                        <div className="pay-card-text-h1 sm-fs-1">
+                            2 месяца
+                        </div>
+                        <div className="pay-card-text-h3 sm-fs-0">
+                            онлайн-обучение
+                        </div>
+                    </div>
+                    <div className="pay-card-item-devider">
+                        <img src={line} alt=""/>
+                    </div>
+                </div>
+                <div className="pay-card-item row g-0 col-4">
+                    <div className="column col-12">
+                        <div className="pay-card-text-h1 sm-fs-1">
+                            оплата
+                        </div>
+                        <div className="pay-card-text-h3 sm-fs-0">
+                            рассрочка платежа
+                        </div>
+                    </div>
+                    <div className="pay-card-item-devider">
+                        <img src={line} alt=""/>
+                    </div>
+                </div>
             </div>
         </div>
     );
