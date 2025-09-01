b957ab062b101f0e944c6ea7dc29b625b4cdbfee
Nighttrolley <gairbekov086@gmail.com>
2025-09-01 13:09:05 +0300

    add cars in fleet list

--- src/config/fleet.data.jsx
+++ src/config/fleet.data.jsx
@@ -8,37 +8,37 @@ export const fleetData = [
     },
     {
         id: 2,
-        car_name: "KIA RIO",
-        gearbox: "Автомат",
-        year: "2021 г.",
-        img: "/icons/image53.svg"
+        car_name: "SKODA OKTAVIA",
+        gearbox: "Механика",
+        year: "2020 г.",
+        img: "/icons/skoda.svg"
     },
     {
         id: 3,
-        car_name: "KIA RIO",
+        car_name: "LADA VESTA",
         gearbox: "Автомат",
-        year: "2021 г.",
-        img: "/icons/image53.svg"
+        year: "2018 г.",
+        img: "/icons/lada.svg"
     },
     {
         id: 4,
-        car_name: "KIA RIO",
-        gearbox: "Автомат",
-        year: "2021 г.",
-        img: "/icons/image53.svg"
+        car_name: "LADA VESTA",
+        gearbox: "Механика",
+        year: "2019 г.",
+        img: "/icons/lada.svg"
     },
     {
         id: 5,
-        car_name: "KIA RIO",
+        car_name: "HYUNDAI CRETA",
         gearbox: "Автомат",
         year: "2021 г.",
-        img: "/icons/image53.svg"
+        img: "/icons/creta.svg"
     },
     {
         id: 6,
-        car_name: "KIA RIO",
-        gearbox: "Автомат",
-        year: "2021 г.",
-        img: "/icons/image53.svg"
+        car_name: "HYUNDAI SOLARIS",
+        gearbox: "Механика",
+        year: "2020 г.",
+        img: "/icons/solaris.svg"
     },
 ]
