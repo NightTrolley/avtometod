226067e62ec07a3dcd020fc3892dfb026986b70c
Nighttrolley <gairbekov086@gmail.com>
2025-09-11 04:07:18 +0300

    some changes

--- src/components/Forms/mainForm.component.js
+++ src/components/Forms/mainForm.component.js
@@ -6,6 +6,7 @@ import "./main.form.css"
 import {useQuiz} from "../../Context/quizContext";
 import axios from "axios";
 import {formSendHook, sendFormData} from "../../hooks/formSendHook";
+import {InputMask} from "@react-input/mask";
 
 const MainFormComponent = ({button_text}) => {
 
@@ -21,79 +22,36 @@ const MainFormComponent = ({button_text}) => {
     });
 
 
-    const onSubmit = async (data) => {
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData.entries());
 
+        console.log(data);
         await sendFormData(data)
 
     }
 
     return (
         <form className={"how-to-start-form d-flex flex-column"}
-              onSubmit={handleSubmit(onSubmit)}
+              onSubmit={onSubmit}
         >
-            <div className="">
-                <input className="col-12"
-                       {...register("your-name", {
-                           required: "Это обязательное поле",
-                           pattern: {
-                               value: /^[А-ЯЁа-яё]+$/,
-                               message: "Поле может содержать только кириллицу"
-                           },
-                           minLength: {
-                               value: 2,
-                               message: "Поле должно содержать не менее 2 символов"
-                           },
-                           maxLength: {
-                               value: 20,
-                               message: "Поле не должно содержать больше 20 символов"
-                           }
-                       })}
-                       placeholder={"Имя"}
+                <input
+                    className="col-12"
+                    required={true}
+                    placeholder={"Имя"}
+                    type={'text'}
+                    name={"your-name"}
                 />
-                <ErrorMessage
-                    errors={errors}
-                    name="your-name"
-                    render={({messages}) => {
-                        return messages
-                            ? Object.entries(messages).map(([type, message]) => (
-                                <p key={type} className="input-error">{message}</p>
-                            ))
-                            : null;
-                    }}
+                <InputMask
+                    mask={"+7 (___) ___-__-__"}
+                    replacement={{ _: /\d/ }}
+                    placeholder={"+7"}
+                    className="col-12"
+                    required={true}
+                    name={"your-phone"}
+                    minLenght={12}
                 />
-            </div>
-            <div className="">
-                <input className="col-12"
-                       maxLength={12}
-                       {...register("your-tel", {
-                           required: "Это обязательное поле",
-                           pattern: {
-                               value: /^\+[1-9]\d{0,3}[\s-]?\(?\d{1,4}\)?[\s-]?\d{1,4}[\s-]?\d{1,4}[\s-]?\d{1,9}$/,
-                               message: "Поле может содержать только цифры"
-                           },
-                           minLength: {
-                               value: 12,
-                               message: "Поле должно содержать не менее 12 символов"
-                           },
-                           maxLength: {
-                               value: 12,
-                               message: "Поле не должно содержать больше 20 символов"
-                           }
-                       })}
-                />
-                <ErrorMessage
-                    errors={errors}
-                    name="your-tel"
-                    render={({messages}) => {
-                        return messages
-                            ? Object.entries(messages).map(([type, message]) => (
-                                <p key={type} className={"input-error"}>{message}</p>
-                            ))
-                            : null;
-                    }}
-                />
-
-            </div>
             <SecondaryButton text={button_text} type={"submit"} isSubmitting={isSubmitting}/>
         </form>
     );
